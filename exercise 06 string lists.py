799e62bb5d8c8f19249600988c60b3df51ffb65f
SandyGCabanes <sandygcabanes@gmail.com>
2019-02-27 15:37:28 +0800

    Add files via upload

---
+++ exercise 06 string lists.py
@@ -0,0 +1,14 @@
+#sandygcabanes
+#practicepython.org
+#exercise 07 list comprehensions
+"""Exercise 7 (and Solution)
+Let’s say I give you a list saved in a variable:
+    a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+    Write one line of Python that takes this list a and makes a new list
+    that has only the even elements of this list in it.
+"""
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+a_even = [num for num in a if num % 2 == 0]
+
+#print (a_even)
